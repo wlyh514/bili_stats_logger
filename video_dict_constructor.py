7462f6d2a4e864f83f808b0351fd2bcecbadc653
wlyh514 <chensy514@gmail.com>
2020-12-29 15:43:17 +0800

    Implemented video_dict_constructor.update_video_dict()

--- video_dict_constructor.py
+++ video_dict_constructor.py
@@ -11,7 +11,10 @@ def new_video_dict(response: dict)-> dict:
         "timestamp": current_timestamp,
 
         "avaliable_time_intervals":[
-
+            {
+                "start": current_timestamp,
+                "end": current_timestamp
+            }
         ],
 
         "popularity": {
@@ -27,7 +30,7 @@ def new_video_dict(response: dict)-> dict:
         },
 
         "semi-statics": {
-            "basis": {
+            "latest": {
                 "title":     response["data"]["title"],
                 "desc":      response["data"]["desc"],
                 "pages":     [page["part"] for page in response["data"]["pages"]],
@@ -47,10 +50,62 @@ def new_video_dict(response: dict)-> dict:
             "coin":     response["data"]["stat"]["coin"],
             "share":    response["data"]["stat"]["share"],
             "like":     response["data"]["stat"]["like"]
+        },
+
+        "dynamics": {
+            "start": current_timestamp,
+            "end":   current_timestamp,
+            "basis": {
+                "view":     response["data"]["stat"]["view"],
+                "danmaku":  response["data"]["stat"]["danmaku"],
+                "reply":    response["data"]["stat"]["reply"],
+                "favorite": response["data"]["stat"]["favorite"],
+                "coin":     response["data"]["stat"]["coin"],
+                "share":    response["data"]["stat"]["share"],
+                "like":     response["data"]["stat"]["like"]
+            },
+            "changes":[
+
+            ]
         }
     }
 
     return video_dict
 
 def update_video_dict(response: dict, video_dict: dict)-> None:
-    pass
+    current_timestamp = int( time() )
+    # Update timestamps
+    video_dict["timestamp"] = current_timestamp
+    video_dict["avaliabe_time_intervals"][-1]["end"] = current_timestamp
+    video_dict["dynamics"]["end"] = current_timestamp
+
+    # Update semi-statics
+    new_semi_statics = {
+        "title":     response["data"]["title"],
+        "desc":      response["data"]["desc"],
+        "pages":     [page["part"] for page in response["data"]["pages"]],
+        "pic":       response["data"]["pic"],
+        "ownerface": response["data"]["owner"]["face"]
+    }
+    for field_name in new_semi_statics:
+        if new_semi_statics[field_name] != \
+            video_dict["semi-statics"]["latest"][field_name]:
+            changelog = {
+                "timestamp": current_timestamp,
+                "field_name": field_name,
+                "old": video_dict["semi-statics"]["latest"][field_name],
+                "new": new_semi_statics[field_name]
+            }
+            video_dict["semi-statics"]["changes"].append(changelog)
+            video_dict["semi-statics"]["latest"][field_name] = new_semi_statics[field_name]
+    
+    # Update dynamics
+    changelog = {
+        "timestamp": current_timestamp
+    }
+    for field_name in video_dict["stats"]:
+        diff = response["data"]["stat"][field_name] - video_dict["stats"][field_name]
+        if diff != 0:
+            changelog[field_name] = diff
+            video_dict["stats"][field_name] += diff
+    video_dict["dynamics"]["changes"].append(changelog)
